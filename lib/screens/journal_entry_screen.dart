48d8db6fe66003a1a265f1a1d2304ba0f0b425dc
vdr <valeria.do@gmail.com>
2022-06-13 13:32:22 -0500

    cleanup code

--- lib/screens/journal_entry_screen.dart
+++ lib/screens/journal_entry_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:pregnancy_journal_m1/constants/constants.dart';
 import 'package:pregnancy_journal_m1/constants/strings.dart';
